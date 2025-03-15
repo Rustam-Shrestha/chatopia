890dde8bb78118188a4921b084a5f97c7d5d1f8d
Rustam Shrestha <santosh.787402@smc.tu.edu.np>
2025-03-15 21:29:27 +0545

    created signup functionality and cookie based signup where one page refresh signs us out and some problems have been fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,17 +7,22 @@ import authRoutes from "../routes/auth.route.js";
 import { connectDB } from "../lib/db.js";
 import Message from "../models/message.model.js";
 import messageRoutes from "../routes/message.route.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Default to port 3000 if PORT is not defined
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true   
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT , () => {
     console.log(`Running on https://localhost:${PORT}`);
     connectDB()
 });
