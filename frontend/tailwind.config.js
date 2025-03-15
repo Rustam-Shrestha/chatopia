6bdfa9673f49a4742f0e90f3b0f18cf08d716762
Rustam Shrestha <santosh.787402@smc.tu.edu.np>
2025-03-15 11:44:08 +0545

    setup project with basic routes daisyui tailwind configuration etc

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"], // Adjust path as needed
+    theme: {
+      extend: {},
+    },
+    plugins: [daisyui],
+  }
+  
